4d31fd4aa86a7c71ecb6be4a9a59677b4c540dad
pcdapichau <163698299+pcdapichau@users.noreply.github.com>
2025-01-13 21:07:53 -0300

    troca das fotos

---
+++ fffb296e8275b9ec.pt-BR.js
@@ -0,0 +1,2 @@
+window['cmsg']['assets'] = window['cmsg']['assets'] || {};
+window['cmsg']['assets']["pt-BR"] = Object.assign(window['cmsg']['assets']["pt-BR"] || {}, {"5706":{"js":["5aeb0039f154a6e6.strings.js"],"css":[]},"8701":{"js":["d27c0ffd95ef3e97.strings.js"],"css":[]}});
